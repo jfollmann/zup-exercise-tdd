4b965f06025142a1e44a549f22b6f67a15db5fa7
Jefferson Follmann <jeff.follmann@gmail.com>
2022-03-31 17:04:20 -0300

    Implemented rule for multiples of 3 and 5

--- tests/fizz-buzz.spec.ts
+++ tests/fizz-buzz.spec.ts
@@ -3,8 +3,13 @@ const fizzBuss = (size: number): any => {
 
   for (let i = 1; i <= size; i++) {
     let content = i.toString()
-    if (i % 3 === 0) { content = 'Fizz' }
-    if (i % 5 === 0) { content = 'Buzz' }
+    if (i % 15 === 0) {
+      content = 'FizzBuzz'
+    } else if (i % 3 === 0) {
+      content = 'Fizz'
+    } else if (i % 5 === 0) {
+      content = 'Buzz'
+    }
 
     response.push(content)
   }
@@ -25,9 +30,15 @@ describe('Fizz Buss Spec', () => {
     expect(result).toEqual(['1', '2', 'Fizz'])
   })
 
-  test('Check if multiples of 5 return "Fizz" word', () => {
+  test('Check if multiples of 5 return "Buzz" word', () => {
     const result = fizzBuss(5)
 
     expect(result).toEqual(['1', '2', 'Fizz', '4', 'Buzz'])
+  })
+
+  test('Check if multiples of 3 and 5 return "FizzBuzz" word', () => {
+    const result = fizzBuss(15)
+
+    expect(result).toEqual(['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz'])
   })
 })
