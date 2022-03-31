91d6a9d72ab9bdaadde22a5b16ae40839fb7f173
Jefferson Follmann <jeff.follmann@gmail.com>
2022-03-31 17:05:51 -0300

    Move fizzBuzz to production code

---
+++ src/fizzBuzz.ts
@@ -0,0 +1,18 @@
+export const fizzBuss = (size: number): any => {
+  const response = []
+
+  for (let i = 1; i <= size; i++) {
+    let content = i.toString()
+    if (i % 15 === 0) {
+      content = 'FizzBuzz'
+    } else if (i % 3 === 0) {
+      content = 'Fizz'
+    } else if (i % 5 === 0) {
+      content = 'Buzz'
+    }
+
+    response.push(content)
+  }
+
+  return response
+}
