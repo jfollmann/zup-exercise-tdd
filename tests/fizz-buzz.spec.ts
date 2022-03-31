a4bceca10abd0dacde679ecc52f721813ac8d8bb
Jefferson Follmann <jeff.follmann@gmail.com>
2022-03-31 16:35:42 -0300

    Call function with a correct params

---
+++ tests/fizz-buzz.spec.ts
@@ -0,0 +1,11 @@
+const fizzBuss = (): any => {
+  return []
+}
+
+describe('Fizz Buss Spec', () => {
+  test('Call function with a correct params', () => {
+    const result = fizzBuss()
+
+    expect(result).toEqual([])
+  })
+})
