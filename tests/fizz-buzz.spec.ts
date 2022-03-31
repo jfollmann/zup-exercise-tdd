05087ed94165b3bbe703326aefdbd6001e149c25
Jefferson Follmann <jeff.follmann@gmail.com>
2022-03-31 17:00:15 -0300

    Implemented rule for multiples of 3

--- tests/fizz-buzz.spec.ts
+++ tests/fizz-buzz.spec.ts
@@ -1,11 +1,26 @@
-const fizzBuss = (): any => {
-  return []
+const fizzBuss = (size: number): any => {
+  const response = []
+
+  for (let i = 1; i <= size; i++) {
+    let content = i.toString()
+    if (i % 3 === 0) { content = 'Fizz' }
+
+    response.push(content)
+  }
+
+  return response
 }
 
 describe('Fizz Buss Spec', () => {
   test('Call function with a correct params', () => {
-    const result = fizzBuss()
+    const result = fizzBuss(0)
 
     expect(result).toEqual([])
+  })
+
+  test('Check if multiples of 3 return "Fizz" word', () => {
+    const result = fizzBuss(3)
+
+    expect(result).toEqual(['1', '2', 'Fizz'])
   })
 })
