468bb598fac98ad9cecd338868db31bca5c8fe39
Jefferson Follmann <jeff.follmann@gmail.com>
2022-04-01 11:34:22 -0300

    roman: create test to check invalid value

--- tests/domain/roman.spec.ts
+++ tests/domain/roman.spec.ts
@@ -1,4 +1,8 @@
 const convertToRoman = (value: number): string => {
+  if (value === undefined) {
+    throw new Error('Invalid value')
+  }
+
   return 'I'
 }
 
@@ -7,5 +11,11 @@ describe('Roman Numbers Spec', () => {
     const result = convertToRoman(1)
 
     expect(result).toEqual('I')
+  })
+
+  test('Call function without value throw error', () => {
+    const fn = convertToRoman
+
+    expect(() => fn(undefined as any)).toThrow(new Error('Invalid value'))
   })
 })
