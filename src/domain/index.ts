e5598880c6f463ad6c04255caec2a04a9bec7290
Jefferson Follmann <jeff.follmann@gmail.com>
2022-04-01 14:43:35 -0300

    Move roman function to production code

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,1 +1,2 @@
 export * from './fizz-buzz'
+export * from './roman-number'
--- tests/domain/roman.spec.ts
+++ tests/domain/roman.spec.ts
@@ -1,36 +1,4 @@
-type RomanNumbers = { key: string, value: number }
-
-const convertToRoman = (value: number): string => {
-  if (value === undefined) {
-    throw new Error('Invalid value')
-  }
-
-  const map: RomanNumbers[] = [
-    { key: 'M', value: 1000 },
-    { key: 'CM', value: 900 },
-    { key: 'D', value: 500 },
-    { key: 'CD', value: 400 },
-    { key: 'C', value: 100 },
-    { key: 'XC', value: 90 },
-    { key: 'L', value: 50 },
-    { key: 'XL', value: 40 },
-    { key: 'X', value: 10 },
-    { key: 'IX', value: 9 },
-    { key: 'V', value: 5 },
-    { key: 'IV', value: 4 },
-    { key: 'I', value: 1 }
-  ]
-
-  let result = ''
-  map.forEach(({ key, value: mapValue }) => {
-    const floor = Math.floor(value / mapValue)
-
-    result += key.repeat(floor)
-    value = value % mapValue
-  })
-
-  return result
-}
+import { convertToRoman } from '@/domain'
 
 describe('Roman Numbers Spec', () => {
   test('should return throw if input is invalid', () => {
