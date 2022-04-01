f9b8147f20e66d2332c2613df88fa281ff090835
Jefferson Follmann <jeff.follmann@gmail.com>
2022-04-01 14:47:50 -0300

    Add sample to call roman numbers (main layer)

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,19 @@
 import './config/module-alias'
 
-import { fizzBuzz } from '@/domain'
+import { convertToRoman, fizzBuzz } from '@/domain'
 
-console.log('---- FizzBuzz ----')
-const itens = [10, 20, 30]
-itens.forEach(input => {
-  console.log({ input, output: fizzBuzz(input) })
-})
+{
+  console.log('---- FizzBuzz ----')
+  const inputs = [10, 20, 30]
+  inputs.forEach(input => {
+    console.log({ input, output: fizzBuzz(input) })
+  })
+}
+
+{
+  console.log('---- Roman Numbers ----')
+  const inputs = [12, 23, 49, 54, 180, 230, 501, 1003]
+  inputs.forEach(input => {
+    console.log({ input, output: convertToRoman(input) })
+  })
+}
