114279ca21ab2bd50e3e35b962fcfb73761e24bf
Jefferson Follmann <jeff.follmann@gmail.com>
2022-04-01 07:54:41 -0300

    Refactory FizzBuzz logic & Tests

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,10 +1,16 @@
 export const fizzBuzz = (size: number): any => {
-  return Array.from({ length: size + 1 }, (x, i) => {
+  if (size === undefined) {
+    throw new Error('')
+  }
+
+  const result = Array.from({ length: size + 1 }, (_, i) => {
     if (i === 0) return ''
     if (i % 15 === 0) return 'FizzBuzz'
     if (i % 3 === 0) return 'Fizz'
     if (i % 5 === 0) return 'Buzz'
 
     return i.toString()
-  }).slice(1)
+  })
+
+  return result.slice(1)
 }
--- tests/fizz-buzz.spec.ts
+++ tests/fizz-buzz.spec.ts
@@ -7,6 +7,12 @@ describe('Fizz Buss Spec', () => {
     expect(result).toEqual([])
   })
 
+  test('Call function without size throw error', () => {
+    const fn = fizzBuzz
+
+    expect(() => fn(undefined as any)).toThrow(new Error())
+  })
+
   test('Check if multiples of 3 return "Fizz" word', () => {
     const result = fizzBuzz(3)
 
@@ -16,12 +22,38 @@ describe('Fizz Buss Spec', () => {
   test('Check if multiples of 5 return "Buzz" word', () => {
     const result = fizzBuzz(5)
 
-    expect(result).toEqual(['1', '2', 'Fizz', '4', 'Buzz'])
+    expect(result).toEqual([
+      '1', '2', 'Fizz',
+      '4', 'Buzz'
+    ])
   })
 
   test('Check if multiples of 3 and 5 return "FizzBuzz" word', () => {
     const result = fizzBuzz(15)
 
-    expect(result).toEqual(['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz'])
+    expect(result).toEqual([
+      '1', '2', 'Fizz',
+      '4', 'Buzz', 'Fizz',
+      '7', '8', 'Fizz',
+      'Buzz', '11', 'Fizz',
+      '13', '14', 'FizzBuzz'])
+  })
+
+  test('Check if result is correct', () => {
+    const result = fizzBuzz(30)
+
+    expect(result).toEqual([
+      '1', '2', 'Fizz',
+      '4', 'Buzz', 'Fizz',
+      '7', '8', 'Fizz',
+      'Buzz', '11', 'Fizz',
+      '13', '14', 'FizzBuzz',
+      '16', '17', 'Fizz',
+      '19', 'Buzz', 'Fizz',
+      '22', '23', 'Fizz',
+      'Buzz', '26', 'Fizz',
+      '28', '29', 'FizzBuzz'
+    ]
+    )
   })
 })
